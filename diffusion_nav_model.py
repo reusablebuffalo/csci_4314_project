fixed agent strategy that moves up concentration gradient
np.gradient(c,dx,dy) was returning fy as first return (i was capturing as fx,fy)

diffusion_nav_model.py
@@ -62,44 +62,48 @@ class Human(Source):
 
 
 class Agent:
-
-    def __init__(self, x=0, y=0, dx=0.5, mem_internal=25):
+    def __init__(self, x=0, y=0, dx=0.5, mem_internal=25, v_multiplier=5):
         self.pos_x = 0
         self.pos_y = 0
-        self.v = dx*5
+        self.v = dx*v_multiplier
 
-        self.position_mem_interval = mem_internal
-        self.prev_x = np.zeros(self.position_mem_interval)
-        self.prev_y = np.zeros(self.position_mem_interval)
-        self.curr_c = np.zeros(self.position_mem_interval)
-        self.dir_bias_coef = dx  
+        # self.position_mem_interval = mem_internal
+        # self.prev_x = np.zeros(self.position_mem_interval)
+        # self.prev_y = np.zeros(self.position_mem_interval)
+        # self.curr_c = np.zeros(self.position_mem_interval)
+        # self.dir_bias_coef = dx  
         self.curr_px = 0
         self.curr_py = 0
         self.prev_px = 0 
         self.prev_py = 0
-        self.prev_theta = 0
+        # self.prev_theta = 0
         self.curr_c_agent = 0
     
+    # default agent moves up concentration gradient at speed v
     def get_agent_position(self, c, x, y, dx, dy):
-        fx,fy = np.gradient(c,dx,dx)
+        fy,fx = np.gradient(c,dx,dx) # fy before fx
         tt = np.floor(x/dx) == np.floor(self.pos_x/dx)
         ttt = np.floor(y/dy) == np.floor(self.pos_y/dy)
-        indt = tt&ttt
-        if np.linalg.norm([fx[indt], fy[indt]])>0: #0.01
+        indt = tt&ttt # where is agent
+        norm =np.linalg.norm([fx[indt], fy[indt]])
+        if norm>0: #0.01
             self.prev_px = self.curr_px
             self.prev_py = self.curr_py
-            self.curr_px = fx[indt]/np.linalg.norm([fx[indt], fy[indt]])
-            self.curr_py = fy[indt]/np.linalg.norm([fx[indt], fy[indt]])
+            self.curr_px = fx[indt][0]/norm
+            self.curr_py = fy[indt][0]/norm
             self.curr_c_agent  = c[indt]
         else:
             self.curr_px = 0 
             self.curr_py = 0
-        self.pos_x = self.pos_x + self.dir_bias_coef*self.curr_px
-        self.pos_y = self.pos_y + self.dir_bias_coef*self.curr_py
+        self.pos_x = self.pos_x + self.v*self.curr_px
+        self.pos_y = self.pos_y + self.v*self.curr_py
         return self.pos_x, self.pos_y
 
-class Dog(Agent):
+class Curtis(Agent):
     pass
+
+class Ian(Agent):
+    pass # maybe add an agent with momentum
 
 class DiffusionModel:
     # used for saving figure
@@ -126,7 +130,7 @@ class DiffusionModel:
             im_ani = animation.ArtistAnimation(self.fig, self.ims, interval=50, repeat_delay=3000)
             Writer = animation.writers['ffmpeg'] # ['pillow'] can write gifs
             writer = Writer(fps=15, metadata=dict(artist='Me'))
-            im_ani.save(save_name, writer=writer, dpi=200)
+            im_ani.save(save_name, writer=writer, dpi=dpi)
 
 
     def source_propagation(self, save_movie=True, include_agent=True, save_name='animation.mp4', n_sources=250):
@@ -161,7 +165,7 @@ class DiffusionModel:
         agent_start = 20 # start agent at t = 20
 
         c = np.zeros(x.shape)
-        self.fig = plt.figure(figsize=(5,6))
+        self.fig = plt.figure(figsize=(10,12))
         self.ims = []
         for t_i in range(1,len(self.t_array)):
             if(t_i%25 == 0):
@@ -174,12 +178,12 @@ class DiffusionModel:
                 # find current up the gradient direction
                 pass
             if save_movie: # ADD ANOTHER OPTION TO WATCH IN REAL TIME vs just save
-                plot = plt.pcolormesh(x,y,c, vmin=0, vmax=3.5)
+                plot = plt.pcolormesh(x,y,c, vmin=0, vmax=5)
                 title = plt.text(0,max_y+5,f"t={t_i}",size=20,horizontalalignment='center',verticalalignment='baseline')
                 if include_agent and t>agent_start:
                     a_x,a_y = self.agent.get_agent_position(c, x, y,dx,dy)
-                    agent = plt.scatter(a_x,a_y)
-                    self.ims.append([plot,title, agent])
+                    agent_plot = plt.scatter(a_x,a_y, c='black')
+                    self.ims.append([plot,title,agent_plot])
                 self.ims.append([plot,title])
                 # plt.draw()
                 # plt.pause(0.00001)
@@ -191,10 +195,10 @@ class DiffusionModel:
                     curr_c = ((self.A/(curr_t**0.5))*np.exp(-1*(np.power((x-source_x_array[source_i]-(wind_x[source_i]*curr_t)),2)+
                     np.power((y-source_y_array[source_i])-wind_y[source_i]*curr_t,2))/(4*self.D*curr_t)))*(0.5**(curr_t/self.B))
                     c = c + curr_c
-        # plt.colorbar()
+        self.fig.colorbar(plot)
         plt.xlabel('x')
         plt.ylabel('y')
-        self.save(save_name, dpi=300)
+        self.save(save_name, dpi=200)
 
-model = DiffusionModel(source=Source(), endtime=100)
+model = DiffusionModel(source=Source(), agent=Agent(v_multiplier=3), endtime=275)
 model.source_propagation(save_name='animation4.mp4', n_sources=250)